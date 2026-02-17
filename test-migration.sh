aae533a90673d2856e22377061de78330154bd8e
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-17 10:50:02 -0500

    add test script

---
+++ test-migration.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+log_dir=$(date -u +%Y%m%dT%H%M%S)
+mkdir "$log_dir"
+packages=$(cat packages-to-migrate.txt)
+echo "Results are in $log_dir"
+# Clean up any aborted previous generation
+git reset --hard > /dev/null
+git clean -d -f > /dev/null
+
+for pkg in $packages
+do
+  echo "$(date -u +%H:%M:%S) Generating $pkg"
+  mkdir "$log_dir/$pkg"
+  go run ../librarian/cmd/librarian generate "$pkg" > "$log_dir"/"$pkg"/log.txt 2>&1
+  git status > "$log_dir"/"$pkg"/status.txt
+  git diff > "$log_dir"/"$pkg"/diff.txt
+  git reset --hard > /dev/null
+  git clean -d -f > /dev/null
+done
