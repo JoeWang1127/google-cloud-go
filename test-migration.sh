27c64b076fcba8636d7c980ee1f4c7d68f3dfeca
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-17 10:55:52 -0500

    update

--- test-migration.sh
+++ test-migration.sh
@@ -5,6 +5,7 @@ set -e
 log_dir=$(date -u +%Y%m%dT%H%M%S)
 mkdir "$log_dir"
 go work use ../librarian
+go work init
 packages=$(cat packages-to-migrate.txt)
 echo "Results are in $log_dir"
 # Clean up any aborted previous generation
