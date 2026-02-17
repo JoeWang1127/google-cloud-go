7d1e31bdacf2f7c1668da57fd1459cfafaf2ca7c
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2026-02-17 11:07:49 -0500

    go build and mv

--- test-migration.sh
+++ test-migration.sh
@@ -4,19 +4,19 @@ set -e
 
 log_dir=$(date -u +%Y%m%dT%H%M%S)
 mkdir "$log_dir"
-go work use ../librarian
-go work init
 packages=$(cat packages-to-migrate.txt)
 echo "Results are in $log_dir"
 # Clean up any aborted previous generation
 git reset --hard > /dev/null
 git clean -d -f > /dev/null
-
+echo "Build librarian binary"
+go build -C ../librarian -o librarian ./cmd/librarian
+mv ../librarian .
 for pkg in $packages
 do
   echo "$(date -u +%H:%M:%S) Generating $pkg"
   mkdir -p "$log_dir/$pkg"
-  go run ../librarian/cmd/librarian generate "$pkg" > "$log_dir"/"$pkg"/log.txt 2>&1
+  ./librarian generate "$pkg" > "$log_dir"/"$pkg"/log.txt 2>&1
   git status > "$log_dir"/"$pkg"/status.txt
   git diff > "$log_dir"/"$pkg"/diff.txt
   git reset --hard > /dev/null
